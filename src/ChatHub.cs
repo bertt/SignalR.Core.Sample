902b5048f78041d24a92055f9629796ad9cadf0f
bertt <btemme@hotmail.com>
2018-07-03 20:29:11 +0200

    clean things

--- src/ChatHub.cs
+++ src/ChatHub.cs
@@ -5,9 +5,21 @@ namespace SignalRChat
 {
     public class ChatHub : Hub
     {
-        public async Task SendMessage(string user, string message)
+        public override Task OnConnectedAsync()
         {
-            await Clients.All.SendAsync("ReceiveMessage", user, message);
+            Clients.All.SendAsync("broadcastMessage", "system", $"{Context.ConnectionId} joined the conversation");
+            return base.OnConnectedAsync();
+        }
+        public void Send(string name, string message)
+        {
+            // Call the broadcastMessage method to update clients.
+            Clients.All.SendAsync("broadcastMessage", name, message);
+        }
+
+        public override Task OnDisconnectedAsync(System.Exception exception)
+        {
+            Clients.All.SendAsync("broadcastMessage", "system", $"{Context.ConnectionId} left the conversation");
+            return base.OnDisconnectedAsync(exception);
         }
     }
 }
